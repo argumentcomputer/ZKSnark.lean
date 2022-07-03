21fd6ceed8bd791a12378e68a5095856fef5b4fa
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2022-07-02 22:24:17 -0400

    Revert "merge to main (#2)" (#3)
    This reverts commit f5de1715a875bbc1388b29d7f6e95b93ad1c3070.

--- ZkSNARK/ZkSNARK.lean
+++
@@ -1,8 +0,0 @@
-import ZkSNARK.ConstraintSystem
-
-namespace ZkSNARK
-
-def sayHello (name : String) : IO Unit := do
-  println! "Hello {name}!"
-
-end ZkSNARK
--- lakefile.lean
+++
@@ -1,6 +0,0 @@
-import Lake
-open Lake DSL
-
-package ZkSNARK {
-  -- add configuration options here
-}
