252a2c0a4f1f07b11c595c885ef34adec080152e
DanielRrr <daniel.rogozin@serokell.io>
2022-07-11 16:22:40 +0300

    fix some MvXMul.lean lemmas (wip)

--- ZkSNARK/GeneralLemmas/MvXMul.lean
+++ ZkSNARK/GeneralLemmas/MvXMul.lean
@@ -21,13 +21,11 @@ section DecidableEq
 
 variable [DecidableEq σ] (a : σ)
 
--- # TODO
--- lemma coeff_X_mul' (m) (s : σ) (p : MvPolynomial σ R) :
---   coeff m (X s * p) = if s ∈ m.support then coeff (m - finsupp.single s 1) p else 0 := 
--- begin
---   rw mul_comm,
---   rw MvPolynomial.coeff_mul_X',
--- end
+--lemma coeff_X_mul' (m) (s : σ) (p : MvPolynomial σ R) :
+--   coeff m (X s * p) = if s ∈ m.support then coeff (m - Finsupp.single s 1) p else 0 := 
+-- by
+--  rw [mul_comm]
+--  rw [MvPolynomial.coeff_mul_X']
 
 end DecidableEq
 
@@ -106,35 +104,24 @@ by sorry -- exact Finset.sum_hom r c
 --   map_zero := monomial_zero,
 -- }
 
--- lemma sum_monomial_hom {α : Type u} {r : Finset α} {f : α -> R}  (s : σ →₀ ℕ) : 
---   ((∑ x in r, monomial s (f x)) : MvPolynomial σ R) = monomial s (∑ x in r, f x)
--- := 
--- begin
---   exact Finset.sum_hom r (monomial s),
--- end
+lemma sum_monomial_hom {α : Type u} {r : Finset α} {f : α -> R}  (s : σ →₀ ℕ) : 
+   ((∑ x in r, monomial s (f x)) : MvPolynomial σ R) = monomial s (∑ x in r, f x)
+ := by sorry -- Finset.mul_sum r (monomial s)
 
 
-
--- lemma extract_mul_from_sum {α : Type u} {r : Finset α} {f : α -> MvPolynomial σ R} (p : MvPolynomial σ R) : 
---   (∑ x in r, p * f x) = p * (∑ x in r, f x)
--- := 
--- begin
---   rw Finset.mul_sum,
--- end
+lemma extract_mul_from_sum {α : Type u} {r : Finset α} {f : α -> MvPolynomial σ R} (p : MvPolynomial σ R) : 
+   (∑ x in r, p * f x) = p * (∑ x in r, f x) := 
+by rw [Finset.mul_sum]
 
 
--- lemma C_mul_C (a a' : R) : C a * C a' = (C (a * a') : MvPolynomial σ R)  := 
--- begin
---   simp,
--- end
+-- lemma C_mul_C (a a' : R) : (C a) * (C a') = (C (a * a') : MvPolynomial σ R) := by simp
 
--- lemma C_mul_monomial' (a a' : R) (s : σ →₀ ℕ) : monomial s a' * C a  = monomial s (a' * a) :=
--- by simp [C_apply, monomial, single_mul_single]
 
--- lemma C_to_monomial (a : R) : @C _ σ _ a = monomial 0 (a) :=
--- begin
---   exact C_apply,
--- end
+-- lemma C_mul_monomial' (a a' : R) (s : σ →₀ ℕ) : (monomial s a') * C a  = monomial s (a' * a) :=
+-- by sorry
+-- simp [C_apply, monomial, single_mul_single]
+
+-- lemma C_to_monomial (a : R) : @C _ σ _ a = monomial 0 (a) := by exact C_apply
 
 -- -- For some reason, this lemma is actually useless https://leanprover.zulipchat.com/#narrow/stream/113488-general/topic/Extracting.20constant.20from.20sum
 -- -- I expect many other lemmas in theis file may be useless as well
@@ -146,98 +133,63 @@ by sorry -- exact Finset.sum_hom r c
 --   rw Finset.sum_hom,
 -- end
 
--- -- lemma rearrange1 (n : ℕ) (v1 v2 : σ) (p : MvPolynomial σ R) : (MvPolynomial.X v1 ^ n) * (MvPolynomial.X v2 * p) = MvPolynomial.X v2 * ((MvPolynomial.X v1 ^ n) * p) :=
--- -- begin
--- --   ring,
--- -- end
+lemma rearrange1 (n : ℕ) (v1 v2 : σ) (p : MvPolynomial σ R) :
+  ((MvPolynomial.x v1) ^ n) * ((MvPolynomial.x v2) * p) = (MvPolynomial.x v2) * ((MvPolynomial.x v1 ^ n) * p) := 
+  by sorry
+  --by ring
 
--- -- lemma rearrange2 (n : ℕ) (f : R) (v1 : σ) (p : MvPolynomial σ R) : (MvPolynomial.X v1 ^ n) * (MvPolynomial.C f * p) = MvPolynomial.C f * ((MvPolynomial.X v1 ^ n) * p) :=
--- -- begin
--- --   ring,
--- -- end
-
--- -- move constants right of X
--- lemma rearrange_constants_right (f : R) (v1 : σ) : 
---  MvPolynomial.C f * MvPolynomial.X v1 = (MvPolynomial.X v1) * (MvPolynomial.C f)
--- :=
--- begin
---   ring,
--- end
-
-
--- lemma rearrange_constants_right_with_extra (f : R) (v1 : σ) (p : MvPolynomial σ R) : 
---  MvPolynomial.C f * (MvPolynomial.X v1 * p) = (MvPolynomial.X v1) * (MvPolynomial.C f * p)
--- :=
--- begin
---   ring,
--- end
-
-
--- lemma rearrange_constants_right_hard (f : R) (p : polynomial R) : 
---   polynomial.C f * p = (p) * (polynomial.C f)
--- :=
--- begin
---   ring,
--- end
-
--- lemma rearrange_sums_right_with_extra {α : Type u} {r : Finset α} {f : α -> MvPolynomial σ R} (s : σ) (p : MvPolynomial σ R) : 
---   (∑ x in r, f x) * (X s * p) = X s * (∑ x in r, f x) * p
--- :=
--- begin
---   ring,
--- end
-
--- lemma rearrange_sums_right {α : Type u} {r : Finset α} {f : α -> MvPolynomial σ R} (s : σ) : 
---   (∑ x in r, f x) * X s = X s * (∑ x in r, f x)
--- :=
--- begin
---   ring,
--- end
+lemma rearrange2 (n : ℕ) (f : R) (v1 : σ) (p : MvPolynomial σ R) :
+  (MvPolynomial.x v1 ^ n) * ((MvPolynomial.c f) * p) = (MvPolynomial.c f) * ((MvPolynomial.x v1 ^ n) * p) := 
+  by sorry
+  -- by ring
 
 -- -- move constants right of X
--- lemma rearrange_smul_right (n : ℕ) (a : R) (v1 : σ) (p : MvPolynomial σ R) : 
---  a • (MvPolynomial.X v1 * p) = (MvPolynomial.X v1) * (a • p)
--- :=
--- begin
---   rw mul_smul_comm,
--- end
+lemma rearrange_constants_right (f : R) (v1 : σ) : 
+  (MvPolynomial.c f) * MvPolynomial.x v1 = (MvPolynomial.x v1) * (MvPolynomial.c f)
+:= by sorry
+-- by ring
 
--- lemma rearrange_001 (f : R) (p1 p2 p3 : polynomial R) : 
---  polynomial.C f * p1 = p2 + p3 ↔ p2 + p3 = polynomial.C f * p1
--- :=
--- begin
---   split,
---   {
---     intro h, rw h,
---   },
---   {
---     intro h, rw h,
---   },
--- end
-
--- lemma rearrange_002 (f : R) (p1 p2 p3 : polynomial R) : 
---  p1 * polynomial.C f  = p2 + p3 ↔ p2 + p3 = p1 * polynomial.C f
--- :=
--- begin
---   split,
---   {
---     intro h, rw h,
---   },
---   {
---     intro h, rw h,
---   },
--- end
+lemma rearrange_constants_right_with_extra (f : R) (v1 : σ) (p : MvPolynomial σ R) : 
+  (MvPolynomial.c f) * ((MvPolynomial.x v1) * p) = (MvPolynomial.X v1) * (MvPolynomial.C f * p)
+:= by ring
 
 
--- lemma add_mul_distrib (a b c d : R) : a + b * c + b * d = a + b * (c + d) :=
--- begin
---   ring,
--- end
+lemma rearrange_constants_right_hard (f : R) (p : Polynomial R) : 
+   Polynomial.c f * p = (p) * (Polynomial.c f)
+:= by ring
 
--- lemma add_mul_distrib' (a b c d : R) : a + c * b + d * b = a + b * (c + d) :=
--- begin
---   ring,
--- end
+lemma rearrange_sums_right_with_extra {α : Type u} {r : Finset α} {f : α → MvPolynomial σ R} (s : σ) (p : MvPolynomial σ R) : 
+   (∑ x in r, f x) * (X s * p) = X s * (∑ x in r, f x) * p
+:= by ring
+
+lemma rearrange_sums_right {α : Type u} {r : Finset α} {f : α → MvPolynomial σ R} (s : σ) : 
+   (∑ x in r, f x) * X s = X s * (∑ x in r, f x)
+:= by ring
+
+-- -- move constants right of X
+lemma rearrange_smul_right (n : ℕ) (a : R) (v1 : σ) (p : MvPolynomial σ R) : 
+  a • (MvPolynomial.x v1 * p) = (MvPolynomial.x v1) * (a • p)
+:= by rw [mul_smul_comm]
+
+lemma rearrange_001 (f : R) (p1 p2 p3 : Polynomial R) : 
+  (Polynomial.c f) * p1 = p2 + p3 ↔ p2 + p3 = (Polynomial.c f) * p1 := by
+   split
+    { intro h, rw h }
+    { intro h, rw h }
+
+lemma rearrange_002 (f : R) (p1 p2 p3 : Polynomial R) : 
+  p1 * (Polynomial.c f) = p2 + p3 ↔ p2 + p3 = p1 * (Polynomial.c f) := by
+   split
+    { intro h, rw h }
+    { intro h, rw h }
+
+variable [CommSemiring R]
+
+lemma add_mul_distrib (a b c d : R) : a + b * c + b * d = a + b * (c + d) :=
+by ring
+
+lemma add_mul_distrib' (a b c d : R) : a + c * b + d * b = a + b * (c + d) :=
+by ring
 
 end CommSemiringₓ
 
