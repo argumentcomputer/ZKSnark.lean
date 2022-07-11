6ad570db838d150d6705506ef81fb39ee4646bd3
Hanting Zhang <hantingz@usc.edu>
2022-07-11 09:41:34 -0700

    sorries

--- ZkSNARK/GeneralLemmas/PolynomialDegree.lean
+++ ZkSNARK/GeneralLemmas/PolynomialDegree.lean
@@ -8,17 +8,19 @@ variable {F : Type u}
 variable [Field F]
 
 lemma natDegreeProductForm (m : ℕ) (f : Finₓ m → F) : 
-  Polynomial.natDegree (∏ i in (Finset.finRange m), (Polynomial.x - Polynomial.c (f i))) = m := by
-  -- rw [t]
-  rw [Polynomial.nat_degree_prod]
-  simp
-  intros i hi
-  exact Polynomial.X_sub_C_ne_zero (f i)
+  Polynomial.natDegree (∏ i in Finset.finRange m, Polynomial.x - Polynomial.c (f i)) = m := by
+  sorry
+  -- rw t,
+  -- rw [Polynomial.nat_degree_prod]
+  -- simp
+  -- intros i hi
+  -- exact Polynomial.X_sub_C_ne_zero (f i)
 
 lemma monicOfProductForm (m : ℕ) (f : Finₓ m → F) : 
-  (∏ i in (Finset.range m), (Polynomial.x - Polynomial.c (f i))).Monic := by
-  apply Polynomial.monic_prod_of_monic
-  intros i hi
-  exact Polynomial.monic_X_sub_C (f i)
+  (∏ i in Finset.finRange m, (Polynomial.x : Polynomial F) - Polynomial.c (f i)).Monic := by
+  sorry
+  -- apply Polynomial.monic_prod_of_monic
+  -- intros i hi
+  -- exact Polynomial.monic_X_sub_C (f i)
 
 end PolynomialDegree
